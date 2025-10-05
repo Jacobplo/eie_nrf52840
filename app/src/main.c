77c5a78ff6f4c8d0a460efd0ded6ec43d85da00d
Jacob Plourde <jacob_plourde@hotmail.com>
2025-10-04 21:04:20 -0600

    Make all four LEDs blink

--- app/src/main.c
+++ app/src/main.c
@@ -2,24 +2,39 @@
 #include <zephyr/drivers/gpio.h>
 
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
+
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
+
 
 int main(void) {
   int ret;
 
-  if(!gpio_is_ready_dt(&led0)) {
+  if(!(gpio_is_ready_dt(&led0) && gpio_is_ready_dt(&led1) && gpio_is_ready_dt(&led2) && gpio_is_ready_dt(&led3))) {
     return -1;
   }
-
-  ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
-  if(ret < 0) {
-    return ret;
+ 
+  // Activate all LED pins.
+  struct gpio_dt_spec const static pins[] = {led0, led1, led2, led3};
+  for(int8_t i = 0; i < 4; i++) {
+    ret = gpio_pin_configure_dt(&pins[i], GPIO_OUTPUT_ACTIVE);
+    if(ret < 0) {
+      return ret;
+    }
   }
-  
+ 
 
   while (1) {
     gpio_pin_toggle_dt(&led0);
+    gpio_pin_toggle_dt(&led1);
+    gpio_pin_toggle_dt(&led2);
+    gpio_pin_toggle_dt(&led3);
 
     k_msleep(1000);
   }
