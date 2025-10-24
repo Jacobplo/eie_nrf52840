f5f186e65bac5a034908fce99424079cb93acbe3
Jacob Plourde <jacob_plourde@hotmail.com>
2025-10-23 19:17:29 -0600

    State machine for blinking LED0

--- app/src/main.c
+++ app/src/main.c
@@ -11,11 +11,11 @@
 
 #include "BTN.h"
 #include "LED.h"
+#include "my_state_machine.h"
 
 #define SLEEP_MS 1
 
 int main(void) {
-
   if (0 > BTN_init()) {
     return 0;
   }
@@ -23,7 +23,14 @@ int main(void) {
     return 0;
   }
 
+  state_machine_init();
+
   while(1) {
+    int ret = state_machine_run();
+    if(ret < 0) {
+      return 0;
+    }
+
     k_msleep(SLEEP_MS);
   }
 	return 0;
