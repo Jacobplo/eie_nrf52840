43e0bee84381710d46428ea3a5740429af2f28cc
Jacob Plourde <jacob_plourde@hotmail.com>
2026-01-22 20:09:14 -0700

    Finish SPI setup

--- app/src/main.c
+++ app/src/main.c
@@ -4,6 +4,7 @@
 
 #include <inttypes.h>
 
+#include <stdint.h>
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/spi.h>
@@ -12,6 +13,7 @@
 
 #include "BTN.h"
 #include "LED.h"
+#include "zephyr/devicetree.h"
 
 
 #define SLEEP_MS 1
@@ -19,11 +21,45 @@
 #define ARDUINO_SPI_NODE DT_NODELABEL(arduino_spi)
 #define ZEPHYR_USER_NODE DT_PATH(zephyr_user)
 
+#define CMD_SOFTWARE_RESET 0x01
+#define CMD_SLEEP_OUT 0x11
+#define CMD_DISPLAY_ON 0x29
+#define CMD_COLUMN_ADDRESS_SET 0x2A
+#define CMD_ROW_ADDRESS_SET 0x2B
+#define CMD_MEMORY_WRITE 0x2C
+
+
 static const struct gpio_dt_spec dcx_gpio = GPIO_DT_SPEC_GET(ZEPHYR_USER_NODE, dcx_gpios);
-//static const struct spi_cs_control cs_ctrl = (struct spi_cs_control){
-//  .gpio = GPIO_DT_SPEC_GET(ARDUINO_SPI_NODE, cs_gpios),
-//  .delay = 0u,
-//};
+static const struct spi_cs_control cs_ctrl = (struct spi_cs_control) {
+  .gpio = GPIO_DT_SPEC_GET(ARDUINO_SPI_NODE, cs_gpios),
+  .delay = 0u,
+};
+
+static const struct device *dev = DEVICE_DT_GET(ARDUINO_SPI_NODE);
+static const struct spi_config spi_cfg = {
+  .frequency = 1000000,
+  .operation = SPI_OP_MODE_MASTER | SPI_WORD_SET(8) | SPI_TRANSFER_MSB,
+  .slave = 0,
+  .cs = cs_ctrl,
+};
+
+
+static void lcd_cmd(uint8_t cmd, struct spi_buf *data) {
+  struct spi_buf cmd_buf[1] = {[0] = {.buf=&cmd, .len=1}};
+  struct spi_buf_set cmd_set = {.buffers=cmd_buf, .count=1};
+
+  gpio_pin_set_dt(&dcx_gpio, 0);
+
+  spi_write(dev, &spi_cfg, &cmd_set);
+
+  if(data != NULL) {
+    struct spi_buf_set data_set = {.buffers=data, .count=1};
+
+    gpio_pin_set_dt(&dcx_gpio, 1);
+
+    spi_write(dev, &spi_cfg, &data_set);
+  }
+}
 
 
 int main(void) {
@@ -41,6 +77,11 @@ int main(void) {
     return 0;
   }
 
+  lcd_cmd(CMD_SOFTWARE_RESET, NULL);
+  k_msleep(120);
+
+  lcd_cmd(CMD_SLEEP_OUT, NULL);
+  lcd_cmd(CMD_DISPLAY_ON, NULL);
 
 
   while(1) {
